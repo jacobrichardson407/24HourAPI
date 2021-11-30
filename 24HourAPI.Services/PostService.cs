72d4c32c667888876b18cd4fd8390a37b4157dfc
Ethan Davis <ethanmd103@gmail.com>
2021-11-30 16:28:31 -0500

    Commit

---
+++ 24HourAPI.Services/PostService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24HourAPI.Services
+{
+    public class PostService
+    {
+        // POST(Create a Post)
+        // GET ALL Posts
+        // POST (Create a Comment on a Post using a Foreign Key relationship)
+        // GET Comments By Post Id
+        // POST(Create) a Reply to a Comment using a Foreign Key relationship
+        // GET Replies By Comment Id 
+
+    }
+}
