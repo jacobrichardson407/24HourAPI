3d03358862c8924c6a07538239aa5a42f071c4b4
Jacob Richardson <jacobrichardson407@gmail.com>
2021-11-30 16:04:54 -0500

    Post and Comment Class finished

--- 24HourAPI_Data/Comment.cs
+++ 24HourAPI_Data/Comment.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +10,11 @@ namespace _24HourAPI_Data
     public class Comment
     {
         public int CommentId { get; set; }
-        public string Title { get; set; }
         public string Text { get; set; }
-        public virtual List<Comment> Comments { get; set; }
-        public Guid MyProperty { get; set; }
+        public Guid AuthorId { get; set; }
+        public virtual List<Reply> Replies { get; set; }
+        [ForeignKey("PostId")]
+        public int PostId { get; set; }
+        public virtual Post Post { get; set; }
     }
 }
